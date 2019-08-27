879aa924c13e89cca0432b14538f3dd5f3b4723d
Xiao <xiao@scp-079.org>
2019-08-27 07:24:40 +0000

    - 基本变量

--- main.py
+++ main.py
@@ -18,3 +18,43 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from telegram.ext import Updater
+
+from plugins import glovar
+from plugins.functions.timers import backup_files, reset_data, send_count, update_admins, update_status
+from plugins.handlers.command import handler_command
+from plugins.handlers.error import handler_error
+from plugins.handlers.message import handler_message
+
+# Enable logging
+logger = logging.getLogger(__name__)
+
+# Create the EventHandler
+updater = Updater(
+    token=glovar.bot_token,
+    request_kwargs=glovar.request_kwargs
+)
+
+# Register handlers
+handler_command(updater.dispatcher)
+handler_error(updater.dispatcher)
+handler_message(updater.dispatcher)
+
+# Timer
+scheduler = BackgroundScheduler()
+scheduler.add_job(update_status, "cron", [updater.bot], minute=30)
+scheduler.add_job(backup_files, "cron", [updater.bot], hour=20)
+scheduler.add_job(send_count, "cron", [updater.bot], hour=21)
+scheduler.add_job(reset_data, "cron", day=glovar.reset_day, hour=22)
+scheduler.add_job(update_admins, "cron", [updater.bot], hour=22, minute=30)
+scheduler.start()
+
+# Start the Bot
+updater.start_polling()
+
+# Run the bot until press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
+updater.idle()
