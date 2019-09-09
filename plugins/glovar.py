b90445fa4660b5fcab350b2507bc1b3320d0a551
Xiao <xiao@scp-079.org>
2019-09-09 06:36:40 +0000

    - 0.0.3

--- plugins/glovar.py
+++ plugins/glovar.py
@@ -81,13 +81,13 @@ locks: Dict[str, Lock] = {
 
 receivers: Dict[str, List[str]] = {
     "bad": ["ANALYZE", "APPEAL", "AVATAR", "CAPTCHA", "CLEAN", "LANG", "LONG",
-            "NOFLOOD", "NOPORN", "NOSPAM", "MANAGE", "RECHECK", "USER", "WATCH"],
+            "MANAGE", "NOFLOOD", "NOPORN", "NOSPAM", "RECHECK", "USER", "WATCH"],
     "declare": ["ANALYZE", "AVATAR", "CLEAN", "LANG", "LONG",
                 "NOFLOOD", "NOPORN", "NOSPAM", "RECHECK", "USER", "WATCH"],
     "score": ["ANALYZE", "CAPTCHA", "CLEAN", "LANG", "LONG",
-              "NOFLOOD", "NOPORN", "NOSPAM", "MANAGE", "RECHECK"],
+              "MANAGE", "NOFLOOD", "NOPORN", "NOSPAM", "RECHECK"],
     "watch": ["ANALYZE", "CAPTCHA", "CLEAN", "LANG", "LONG",
-              "NOFLOOD", "NOPORN", "NOSPAM", "MANAGE", "RECHECK", "WATCH"]
+              "MANAGE", "NOFLOOD", "NOPORN", "NOSPAM", "RECHECK", "WATCH"]
 }
 
 recorded_ids: Dict[int, Set[int]] = {}
