a8b51410e72b182bab7cff9b8820b92aaecc62b3
Xiao <xiao@scp-079.org>
2019-10-07 08:19:46 +0000

    - 0.0.6

--- plugins/functions/etc.py
+++ plugins/functions/etc.py
@@ -311,9 +311,10 @@ def random_str(i: int) -> str:
 
 
 def t2s(text: str) -> str:
-    # Convert Traditional Chinese to Simplified Chinese
+    # Convert the string
     try:
-        text = convert(text, config="t2s.json")
+        if glovar.zh_cn:
+            text = convert(text, config="t2s.json")
     except Exception as e:
         logger.warning(f"T2S error: {e}", exc_info=True)
 
